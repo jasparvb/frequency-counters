96e347c6d3f3b1170d3b96d30b0ffcf26c3438d2
von Buelow <jaspar@UMICH.EDU>
2020-08-30 18:44:44 -0400

    average-pair fn

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,11 @@
 // add whatever parameters you deem necessary
 function averagePair(arr, avg) {
+  let i = 0;
+  let j = arr.length - 1;
+  while(i < j) {
+    if((arr[i] + arr[j]) / 2 === avg) return true;
+    if((arr[i] + arr[j]) / 2 < avg) i++;
+    else j--;
+  }
+  return false;
 }
