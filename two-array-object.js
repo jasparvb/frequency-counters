d0df32a362055a681c1dbe9137d5c80bdd6b3bc3
von Buelow <jaspar@UMICH.EDU>
2020-08-30 19:21:19 -0400

    Two array object fn

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,8 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(arr1, arr2) {
+  let newObj = {};
+  for(let i = 0; i < arr1.length; i++) {
+    newObj[arr1[i]] = arr2[i] || null;
+  }
+  return newObj;
 }
