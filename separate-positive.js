9b7706aeaad7b6ec42182ef88788cb517b2d02bc
von Buelow <jaspar@UMICH.EDU>
2020-08-30 19:53:15 -0400

    Separate positive fn

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,13 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(arr) {
+  let i = 0;
+  let j = arr.length - 1;
+  while(i < j) {
+    if(arr[i] < 0 && arr[j] > 0) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    if(arr[i] > 0) i++;
+    if(arr[j] < 0) j--;
+  }
+  return arr;
 }
