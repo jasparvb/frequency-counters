b74a2fcca1b8cf036a3f3110dc0765f5605603a8
von Buelow <jaspar@UMICH.EDU>
2020-08-30 19:36:37 -0400

    Same frequency fn

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,18 @@
 // add whatever parameters you deem necessary
-function sameFrequency() {
+function frequencyCounter(str) {
+  let data = {};
+  for(let char of str) {
+    data[char] = data[char] + 1 || 1;
+  }
+  return data;
+}
+
+function sameFrequency(num1, num2) {
+  if (num1.toString().length !== num2.toString().length) return false;
+  const numCount1 = frequencyCounter(num1.toString());
+  const numCount2 = frequencyCounter(num2.toString());
+  for(let key in numCount1) {
+    if(numCount1[key] !== numCount2[key]) return false;
+  }
+  return true;
 }
