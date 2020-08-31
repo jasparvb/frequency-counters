49f39498414643faca1e1829e3e67c392d10d110
von Buelow <jaspar@UMICH.EDU>
2020-08-31 10:06:20 -0400

    Is subsequence fn

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,3 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(arr, val) {
 }
--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,9 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2) {
+  let i = 0;
+  for(let char of str2) {
+    if(char === str1[i]) i++;
+    if(i === str1.length) return true;
+  }
+  return false;
 }
