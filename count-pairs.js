077a2dc6402f02f68265adc6e11daf517baf737a
von Buelow <jaspar@UMICH.EDU>
2020-08-31 10:06:55 -0400

    Count pairs fn

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
 function countPairs(arr, val) {
+  let nums = {};
+  let pairs = 0;
+  for(let num of arr) {
+    if(num * 2 !== val){
+      nums[num] = val - num;
+    }
+  }
+  for(let num of arr) {
+    if(nums[nums[num]]) {
+      delete nums[num];
+      pairs++;
+    };
+  }
+  return pairs;
 }
